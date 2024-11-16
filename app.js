e9865f916a51ad2a337813ec9b234c0ef2abc640
Anshu <anshupr749@gmail.com>
2024-11-16 13:30:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import cookieParser from "cookie-parser";
+import { dbConnection } from "./database/dbConnection.js";
+import messageRouter from "./router/messageRouter.js";
+import {errorMiddleware} from "./middlewares/error.js"
+import userRouter from "./router/userRouter.js"
+
+const app = express();
+config({ path: "./config/.env" });
+
+app.use(
+    cors({
+        origin:[process.env.FRONTEND_URL,process.env.DASHBOARD_URL],
+        methods:["GET","POST","PUT","DELETE"],
+        credential:true,
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+app.use(
+    fileUpload({
+        useTempfile: true,
+        tempFileDir: "/tmp/",
+
+    })
+)
+
+app.use("/api/v1/message",
+    messageRouter());
+app.use("/api/v1/user",
+    userRouter());
+
+dbConnection()
+
+app.use(errorMiddleware());
+export default app;
